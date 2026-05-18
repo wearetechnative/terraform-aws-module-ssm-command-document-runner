5ac5b97b2e64379ad9e44ff4965d335a7b7666f9
jerpen80 <dra_penny@yahoo.com>
2026-05-18 14:22:54 +0200

    added event rule for this module

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 resource "aws_ssm_document" "ssm_doc" {
   name          = var.document_name
   document_type = "Command"
-  content = var.content
+  content       = var.content
 }
 
 resource "aws_ssm_association" "run_ssm_doc" {
@@ -11,7 +15,29 @@ resource "aws_ssm_association" "run_ssm_doc" {
     key    = "InstanceIds"
     values = var.targets
   }
-  schedule_expression = var.schedule
+  schedule_expression         = var.schedule
   apply_only_at_cron_interval = var.apply_only_at_cron_interval
 }
 
+### This most likely will trigger events twice if the normal ssm rule is also enabled
+## TODO: figure out solution for ths
+
+resource "aws_cloudwatch_event_rule" "run_ssm_doc" {
+  name        = "ssm document runner notification rule"
+  description = "Capture each AWS Console Sign In"
+
+  event_pattern = {
+    "source" : ["aws.ssm"],
+    "detail-type" : ["EC2 State Manager Instance Association State Change"],
+    "detail" : {
+      "status" : ["Failed"],
+      "document-name" : [aws_ssm_document.ssm_doc.name]
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = aws_cloudwatch_event_rule.run_ssm_doc.name
+  target_id = "SendToObservability"
+  arn       = "arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:observability-sns-topic"
+}
--- variables.tf
+++ variables.tf
@@ -1,28 +1,28 @@
 variable "schedule" {
-  type = string
-  default = "cron(0 10 ? * * *)"
-	description = "The timezone of cron expression is UTC"
+  type        = string
+  default     = "cron(0 10 ? * * *)"
+  description = "The timezone of cron expression is UTC"
 }
 
 variable "apply_only_at_cron_interval" {
-  type = bool
-  default = true
-	description = "Disable this option if you do not want an association to run immediately after you create or update it. "
+  type        = bool
+  default     = true
+  description = "Disable this option if you do not want an association to run immediately after you create or update it. "
 }
 
 variable "targets" {
-	type = list(string)
-	default = [ "*" ]
-	description = "list of instance ids to be checked"
+  type        = list(string)
+  default     = ["*"]
+  description = "list of instance ids to be checked"
 }
 
 variable "content" {
-	type = string
-	description = "script we need to run on the ssm managed ssm instances"
+  type        = string
+  description = "script we need to run on the ssm managed ssm instances"
 }
 
 
-variable "document_name"{
-	type = string
-	description = "The name you want to give to your ssm document and please make sure that it is descriptive as it will be used in the alerts"
+variable "document_name" {
+  type        = string
+  description = "The name you want to give to your ssm document and please make sure that it is descriptive as it will be used in the alerts"
 }
